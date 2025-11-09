3749599e3f9de1717745726df7432e1bbce737d6
Atakan Akın <atakan.akin@metu.edu.tr>
2025-11-10 00:12:15 +0300

    fix: add trailing commas to color and typography definitions for consistency

--- app/src/main/java/dev/atakanakin/common/showcase/ui/theme/Theme.kt
+++ app/src/main/java/dev/atakanakin/common/showcase/ui/theme/Theme.kt
@@ -20,13 +20,13 @@ import androidx.compose.ui.platform.LocalContext
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
@@ -43,7 +43,7 @@ fun CommonShowcaseTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
+    content: @Composable () -> Unit,
 ) {
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
@@ -58,6 +58,6 @@ fun CommonShowcaseTheme(
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
-        content = content
+        content = content,
     )
 }
